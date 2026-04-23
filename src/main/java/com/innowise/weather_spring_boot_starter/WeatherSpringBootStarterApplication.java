f2097af4a92a0d1e48c77487b2d7a35b9cdf5b5c
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-04-23 13:16:38 +0300

    init

---
+++ src/main/java/com/innowise/weather_spring_boot_starter/WeatherSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.innowise.weather_spring_boot_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innowise/weather_spring_boot_starter/WeatherSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innowise.weather_spring_boot_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
