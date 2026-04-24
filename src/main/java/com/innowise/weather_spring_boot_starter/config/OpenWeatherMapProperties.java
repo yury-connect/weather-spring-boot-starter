0b523d1a19fe27042ef30a20b3da829095dd52ca
Yury Lapitski /VikaNoteBook_Win11 <yury.connect@gmail.com>
2026-04-24 14:07:00 +0300

    обновил код. Все работает.

--- src/main/java/com/innowise/weather_spring_boot_starter/config/OpenWeatherMapProperties.java
+++ src/main/java/com/innowise/weather_spring_boot_starter/config/OpenWeatherMapProperties.java
@@ -1,15 +1,20 @@
 package com.innowise.weather_spring_boot_starter.config;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Setter
+@Validated
 @ConfigurationProperties(prefix = "openweathermap-starter")
 public class OpenWeatherMapProperties {
 
+    @NotBlank(message = "OpenWeatherMap API key is required")
     private String sdkKey;
 
+    @NotBlank(message = "Default city is required")
     private String city;
 }
